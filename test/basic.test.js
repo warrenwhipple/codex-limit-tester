5c5852a713cb12bd9f55ffb3238bc3a8089a309a
Warren Whipple <w@warrenwhipple.com>
2025-05-18 06:39:44 +0100

    Reorganize

--- test/basic.test.js
+++
@@ -1,7 +0,0 @@
-const test = require('node:test');
-const assert = require('node:assert');
-const { add } = require('../index');
-
-test('add adds two numbers', () => {
-  assert.strictEqual(add(1, 1), 2);
-});
